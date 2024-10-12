b1659ddb6c10513155c8078ebf229eff8ec961c2
belikovvvartem <beliko.artem43334@icloud.com>
2024-10-12 12:21:55 +0300

    Version 4
    All sections are ready. Additionally, Glassmorphism was used in the project. In the "Order" section, we added a shadow. We configured the navigation. We added a scroll-to-top button (scrollToTopButton), which also uses Glassmorphism.

--- script.js
+++ script.js
@@ -1,9 +1,29 @@
 document.querySelectorAll('.nav a').forEach(anchor => {
-    anchor.addEventListener('click', function(e) {
+    anchor.addEventListener('click', function (e) {
         e.preventDefault();
         const targetId = this.getAttribute('href');
         document.querySelector(targetId).scrollIntoView({
             behavior: 'smooth'
         });
     });
-});
+})
+
+// Отримати кнопку
+const scrollToTopButton = document.getElementById("scrollToTop");
+
+// Показати кнопку при прокрутці
+window.onscroll = function() {
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        scrollToTopButton.style.display = "block";
+    } else {
+        scrollToTopButton.style.display = "none";
+    }
+};
+
+// Прокрутка до верху
+scrollToTopButton.onclick = function() {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth" // Анімація прокрутки
+    });
+};
