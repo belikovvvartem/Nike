194229d2f2febdfffa42f36746cb2c064ee0ffed
belikovvvartem <beliko.artem43334@icloud.com>
2024-10-20 14:27:59 +0300

    Version 8
    Overall, we corrected all minor issues, but the real cherry on top is that we made the website fully responsive, adapting it to different devices and screen sizes. This allows users to enjoy a seamless browsing experience no matter what device they are using.

--- script.js
+++ script.js
@@ -21,6 +21,73 @@ window.onscroll = function () {
 scrollToTopButton.onclick = function () {
     window.scrollTo({
         top: 0,
-        behavior: "smooth" 
+        behavior: "smooth"
     });
 };
+
+function toggleMenu() {
+    const modal = document.getElementById('navModal');
+    modal.style.display = modal.style.display === "block" ? "none" : "block";
+}
+
+
+window.onclick = function (event) {
+    const modal = document.getElementById('navModal');
+    if (event.target === modal) {
+        modal.style.display = "none";
+    }
+}
+
+
+
+const reviewsWrapper = document.querySelector('.reviews-wrapper');
+const reviewCards = document.querySelectorAll('.review-card');
+let currentIndex = 0;
+
+function updateSliderPosition() {
+    reviewsWrapper.style.transform = `translateX(-${currentIndex * 35}%)`;
+}
+
+document.querySelector('#next-button').addEventListener('click', () => {
+    if (currentIndex < reviewCards.length - 1) {
+        currentIndex++;
+    } else {
+        currentIndex = 0; 
+    }
+    updateSliderPosition();
+});
+
+document.querySelector('#prev-button').addEventListener('click', () => {
+    if (currentIndex > 0) {
+        currentIndex--;
+    } else {
+        currentIndex = reviewCards.length - 1; 
+    }
+    updateSliderPosition();
+});
+
+const advantagesWrapper = document.querySelector('.advantages-wrapper');
+const advantageCards = document.querySelectorAll('.advantage');
+let currentBenefitIndex = 0;
+
+function updateBenefitsSlider() {
+    advantagesWrapper.style.transform = `translateX(-${currentBenefitIndex * 100}%)`;
+}
+
+document.querySelector('#next-benefit').addEventListener('click', () => {
+    if (currentBenefitIndex < advantageCards.length - 1) {
+        currentBenefitIndex++;
+    } else {
+        currentBenefitIndex = 0; 
+    }
+    updateBenefitsSlider();
+});
+
+document.querySelector('#prev-benefit').addEventListener('click', () => {
+    if (currentBenefitIndex > 0) {
+        currentBenefitIndex--;
+    } else {
+        currentBenefitIndex = advantageCards.length - 1;
+    }
+    updateBenefitsSlider();
+});
